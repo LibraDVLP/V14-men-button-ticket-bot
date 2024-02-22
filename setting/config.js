e18fc2205ca02bdfcb74383a9ae0123f9aff5c78
LibraDVLP <129540940+LibraDVLP@users.noreply.github.com>
2024-02-22 21:24:34 +0400

    Create config.js

---
+++ setting/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: (process.env.token),////bot tokeniniz env girilcek VSC yapmak için burayı degismeniz yeter.
+    ticket_channel: "Ticket mesajının gönderileceği kanal idsi",
+    ticket_category: "Ticket Olusturulcak kanal idsi",
+    ticket_logs: "Ticket Log kanal idsi",
+    support_team: "Destek Rolünün İdsi"
+}
